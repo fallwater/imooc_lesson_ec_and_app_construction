9ad93b0fe8e74bac32eab36988b732e0bc6074da
Fallwater <1667376033@qq.com>
2017-11-29 08:13:39 +0800

    1.初始化项目

---
+++ app/src/main/java/com/ifall/fallwater21071129/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ifall.fallwater21071129;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
